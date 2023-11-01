3067bab85dc9c3cfa917b6a22a6f437aec61af87
Sam Faulkner <samfaulkner@Biggie.local>
2023-11-01 11:53:53 +0000

    gave it a try

--- components/profilePage/ProfileMap.tsx
+++ components/profilePage/ProfileMap.tsx
@@ -2,19 +2,53 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MapView from 'react-native-maps'
 import { winHeight, winWidth } from '../../assets/variables/height-width'
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography
+} from 'react-simple-maps'
+import * as Svg from 'react-native-svg'
+import countries from '../../assets/countries-50m.json'
 
 export default function ProfileMap () {
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 0,
-          longitude: -60,
-          latitudeDelta: 180,
-          longitudeDelta: 180
-        }}
-      />
+      <ComposableMap>
+        <ZoomableGroup>
+          <Geographies geography='../../assets/countries-50m.json'>
+            {(geographies, projection) =>
+              geographies.map((geography, i) => (
+                <Geography
+                  key={i}
+                  geography={geography}
+                  projection={projection}
+                  style={{
+                    default: {
+                      fill: '#ECEFF1',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.75,
+                      outline: 'none'
+                    },
+                    hover: {
+                      fill: '#607D8B',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.75,
+                      outline: 'none'
+                    },
+                    pressed: {
+                      fill: '#FF5722',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.75,
+                      outline: 'none'
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
     </View>
   )
 }
